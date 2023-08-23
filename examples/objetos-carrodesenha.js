c7930a959ca0698dce1ec3ea8bfb4f0823e151d0
Guilherme Ranoya <ranoya@gmail.com>
2023-08-23 00:24:55 -0300

    mais um exemplo para o tour

---
+++ examples/objetos-carrodesenha.js
@@ -0,0 +1,50 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(100);
+    noStroke();
+}
+
+// Criação dos carros como objetos
+let carroA = {
+    nome: "Carro 1",
+    velocidade: 5,
+    posicao: 200,
+    anda: function () {
+        this.posicao = this.posicao + this.velocidade;
+    },
+    desenha: function () {
+        fill("#00FF00");
+        rect(this.posicao, 70,30,30);
+        textSize(32);
+        text(this.nome, this.posicao, 50);
+    }
+}
+
+let carroB = {
+    nome: "Carro 2",
+    velocidade: 3,
+    posicao: 0,
+    anda: function () {
+        this.posicao = this.posicao + this.velocidade;
+    },
+    desenha: function () {
+        fill("#FF0000");
+        rect(this.posicao, 70,30,30);
+        textSize(32);
+        text(this.nome, this.posicao, 50);
+    }
+}
+
+function draw() {
+   
+    background(255);
+
+    // Desenha os dois carros
+    carroA.desenha();
+    carroB.desenha();
+
+    // Movimenta os dois carros
+    carroA.anda();
+    carroB.anda();
+
+}
