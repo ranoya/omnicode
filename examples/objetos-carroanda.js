2e93ff4ea090405bd91566794b004f8ec52cdf99
Guilherme Ranoya <ranoya@gmail.com>
2023-08-23 00:04:18 -0300

    mais uma parte do code tour

---
+++ examples/objetos-carroanda.js
@@ -0,0 +1,46 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(100);
+    noStroke();
+}
+
+// Criação dos carros como objetos
+let carroA = {
+    nome: "Carro 1",
+    velocidade: 5,
+    posicao: 200,
+    anda: function () {
+        this.posicao = this.posicao + this.velocidade;
+    }
+}
+
+let carroB = {
+    nome: "Carro 2",
+    velocidade: 3,
+    posicao: 0,
+    anda: function () {
+        this.posicao = this.posicao + this.velocidade;
+    }
+}
+
+function draw() {
+   
+    background(255);
+
+    // Desenha o primeiro carro
+    fill("#00FF00");
+    rect(carroA.posicao, 70,30,30);
+    textSize(32);
+    text(carroA.nome, carroA.posicao, 50);
+
+    // Desenha o segundo carro
+    fill("#FF0000");
+    rect(carroB.posicao, 200,30,30);
+    textSize(32);
+    text(carroB.nome, carroB.posicao, 180);
+
+    // Movimenta os dois carros
+    carroA.anda();
+    carroB.anda();
+
+}
