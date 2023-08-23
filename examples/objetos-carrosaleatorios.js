9f3825d6a2842c9e4d88216914270b0aa96285b7
Guilherme Ranoya <ranoya@gmail.com>
2023-08-23 08:49:46 -0300

    mais um exemplo para o tour

---
+++ examples/objetos-carrosaleatorios.js
@@ -0,0 +1,69 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(100);
+    noStroke();
+}
+
+// Criação dos carros como objetos
+let carroA = {
+    ligado: true,
+    nome: "Carro 1",
+    velocidade: Math.random() * 5 + 2,
+    posicao: Math.random() * 100,
+    y: Math.random() * 200,
+    corR: Math.random() * 255,
+    corG: Math.random() * 255,
+    corB: Math.random() * 255,
+    anda: function () {
+        if (this.ligado == true) {
+            this.posicao = this.posicao + this.velocidade;
+        }
+    },
+    desenha: function () {
+            fill(this.corR, this.corG, this.corB);
+            rect(this.posicao, this.y, 30, 30);
+            textSize(32);
+            text(this.nome, this.posicao, this.y - 20);
+    }
+}
+
+let carroB = {
+    ligado: true,
+    nome: "Carro 2",
+    velocidade: Math.random() * 7 + 1,
+    posicao: Math.random() * 100,
+    y: Math.random() * 200 + 200,
+    corR: Math.random() * 255,
+    corG: Math.random() * 255,
+    corB: Math.random() * 255,
+    anda: function () {
+        if (this.ligado == true) {
+            this.posicao = this.posicao + this.velocidade;
+        }
+    },
+    desenha: function () {
+            fill(this.corR, this.corG, this.corB);
+            rect(this.posicao, this.y, 30, 30);
+            textSize(32);
+            text(this.nome, this.posicao, this.y - 20);
+    }
+}
+
+function draw() {
+   
+    background(255);
+
+    // Desenha os dois carros
+    carroA.desenha();
+    carroB.desenha();
+
+    // Movimenta os dois carros
+    carroA.anda();
+    carroB.anda();
+
+    // Para um dos carros eventualmente
+    if (random(100) > 95) {
+        carroB.ligado = false;
+    }
+
+}
