df6d26c26641063538eab98c1062dca94415b0f7
Guilherme Ranoya <ranoya@gmail.com>
2026-01-03 16:21:21 -0300

    para o exemplo não ficar no vazio

--- examples/p5start.js
+++ examples/p5start.js
@@ -1,10 +1,11 @@
 function setup() {
-    createCanvas(500, 500);
-    print("exemplo original do Processing");
+  createCanvas(500, 500);
+  print("exemplo original do Processing");
+  background(128);
 }
 
 function draw() {
-    fill(255);
-    stroke(0);
-    ellipse(mouseX, mouseY, 30, 30);
+  fill(255);
+  stroke(0);
+  ellipse(mouseX, mouseY, 30, 30);
 }
